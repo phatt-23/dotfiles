df417255225a526cd11a93000896c7b5f852b8c3
phatt-23 <phat.trandai23@gmail.com>
2024-08-31 14:39:15 +0200

    update

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,83 +1,56 @@
 return {
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-			local cmp = require("cmp")
+    { "hrsh7th/cmp-nvim-lsp" },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+        },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+            local cmp = require("cmp")
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				window = {
-					-- completion = cmp.config.window.bordered(),
-					-- documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" }, -- For luasnip users.
+                }, {
+                    { name = "buffer" },
+                }),
+            })
 
-			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-			-- cmp.setup.cmdline({ "/", "?" }, {
-			-- 	mapping = cmp.mapping.preset.cmdline(),
-			-- 	sources = {
-			-- 		{ name = "buffer" },
-			-- 	},
-			-- })
+            local lsp_names = require("plugins.lsp-config").lsp_names
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
 
-			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-			-- cmp.setup.cmdline(":", {
-			-- 	mapping = cmp.mapping.preset.cmdline(),
-			-- 	sources = cmp.config.sources({
-			-- 		{ name = "path" },
-			-- 		{ name = "cmdline" },
-			-- 	}),
-			-- 	matching = { disallow_symbol_nonprefix_matching = false },
-			-- })
+            for _, lsp in ipairs(lsp_names) do
+                lspconfig[lsp].setup({
+                    capabilities = capabilities,
+                })
+            end
 
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			local servers = {
-				"lua_ls",
-				"rust_analyzer",
-				"csharp_ls",
-				"eslint",
-				"tsserver",
-				"html",
-				"cssls",
-			}
+            require("plugins.completion.kinds").setup()
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-
-			for _, lsp in ipairs(servers) do
-				lspconfig[lsp].setup({
-					capabilities = capabilities,
-				})
-			end
-		end,
-	},
+        end,
+    },
 }
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -7,9 +7,11 @@ local lsp_names = {
     "html",
     "cssls",
     "clangd",
+    "sqlls",
 }
 
 return {
+    lsp_names = lsp_names,
     {
         "williamboman/mason.nvim",
         config = function()
@@ -28,12 +30,37 @@ return {
             local lspconfig = require("lspconfig")
 
             for _, lsp_name in ipairs(lsp_names) do
-                lspconfig[lsp_name].setup({})
+                lspconfig[lsp_name].setup({
+                    handlers = handlers,
+                })
             end
 
             vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+            -- Got it from here: https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization
+            -- Specify how the border looks like
+            local border = {
+                { "┌", "FloatBorder" },
+                { "─", "FloatBorder" },
+                { "┐", "FloatBorder" },
+                { "│", "FloatBorder" },
+                { "┘", "FloatBorder" },
+                { "─", "FloatBorder" },
+                { "└", "FloatBorder" },
+                { "│", "FloatBorder" },
+            }
+
+            -- To instead override globally
+            local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+            function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+                opts = opts or {}
+                opts.border = opts.border or border
+                return orig_util_open_floating_preview(contents, syntax, opts, ...)
+            end
+
+
         end,
     },
 }
